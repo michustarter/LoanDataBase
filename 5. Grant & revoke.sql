4b3ecbb66933caedcebd5d01713b0c9932534f8c
michustarter <michal.ratajczyk@capgemini.com>
2018-08-09 17:03:24 +0200

    added modifications

--- 5. Grant & revoke.sql
+++ 5. Grant & revoke.sql
@@ -1,15 +1,15 @@
 
 -- a
-CREATE USER IF NOT EXISTS user_ro IDENTIFIED BY 'password_0';
+CREATE USER IF NOT EXISTS user_ro IDENTIFIED BY 'pass';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM user_ro;
-GRANT SELECT ON car_rental.* TO 'user_ro';
+GRANT SELECT ON car_rental.* TO user_ro;
 
 -- b
-CREATE USER IF NOT EXISTS user_rw IDENTIFIED BY 'password_1';
+CREATE USER IF NOT EXISTS user_rw IDENTIFIED BY 'pass';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM user_rw;
-GRANT SELECT ON car_rental.* TO 'user_rw';
-GRANT INSERT, DELETE ON car_rental.customer  TO 'user_rw';
+GRANT SELECT ON car_rental.* TO user_rw;
+GRANT INSERT, DELETE ON car_rental.customer  TO user_rw;
 
 -- c
-CREATE USER IF NOT EXISTS user_admin IDENTIFIED BY 'password_2';
+CREATE USER IF NOT EXISTS user_admin IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON car_rental.* TO user_admin;
